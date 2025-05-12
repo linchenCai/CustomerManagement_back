edd6e9081016db7bf475ca3948d13e037f6452da
zhangwei43721 <zhangwei4372@outlook.com>
2025-05-12 17:13:04 +0800

    对mapper测试成功

--- src/main/java/com/example/demo/pojo/User.java
+++ src/main/java/com/example/demo/pojo/User.java
@@ -1,7 +1,6 @@
 package com.example.demo.pojo;
 
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
@@ -38,4 +37,5 @@ public class User {
      * 
      */
     private String password;
+
 }
