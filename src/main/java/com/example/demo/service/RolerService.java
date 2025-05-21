da70f620d7352d9e47d3c597a0a7386514317c12
linchenCai <2412394569@qq.com>
2025-05-21 14:53:55 +0800

    feat(roler): 添加角色信息管理功能
    - 新增 RolerController 类，实现角色信息的分页查询、更新、保存和删除功能
    - 在 RolerService接口中添加 queryRolePageListService 方法，用于分页查询角色信息
    - 实现 RolerServiceImpl 类中的 queryRolePageListService 方法，使用 MyBatis-Plus 分页插件进行角色信息的分页查询

--- src/main/java/com/example/demo/service/RolerService.java
+++ src/main/java/com/example/demo/service/RolerService.java
@@ -3,11 +3,14 @@ package com.example.demo.service;
 import com.example.demo.pojo.Roler;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.Map;
+
 /**
 * @author 24123
 * @description 针对表【t_roler】的数据库操作Service
 * @createDate 2025-05-19 15:05:50
 */
 public interface RolerService extends IService<Roler> {
-
+    /*实现角色信息分页查询*/
+    public Map<String,Object> queryRolePageListService(Integer pageNum, Integer pageSize);
 }
